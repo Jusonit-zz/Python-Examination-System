e04ce4324a3a0ccfc744dad8a4f0f671f14d1a49
Junior <juniorsj@live.com>
2019-05-09 00:49:03 -0400

    Add files via upload

---
+++ Beta/FrontEnd/login.php
@@ -0,0 +1,14 @@
+<?php
+
+  $url = "https://web.njit.edu/~bt74/betta/middleware/";
+  $data = $_POST;
+      
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  $response = curl_exec($ch);
+  curl_close ($ch);
+  
+  echo $response; 
+?>
